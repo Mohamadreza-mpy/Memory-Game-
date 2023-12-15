490073099eb2fa56b9d0782e7c4a76ab27053918
mohammadreza <mohammadreza.mir.8448@gmail.com>
2023-12-15 08:38:10 +0330

    first push and fix backend

---
+++ index.js
@@ -0,0 +1,80 @@
+var counterElement = document.getElementById("counter");
+    var counter = 5;
+    var timeoutId;
+    let score = 0;    
+    function getRandomItem(arr) {
+      const randomIndex = Math.floor(Math.random() * arr.length);
+      const item = arr[randomIndex];
+      return item;
+  }
+
+    function changeIcon()
+    {
+      listClassIcons = ['glyphicon-cloud', 'glyphicon-remove', 'glyphicon-user', 'glyphicon-envelope']
+      randomIcon = getRandomItem(listClassIcons);
+
+      let isIcon = document.getElementById('iconChang')
+      // Remove icon
+      isIcon.classList.remove(isIcon.classList[1]);
+      // Add icon
+      isIcon.classList.add(randomIcon);
+    }
+    
+    
+    function startCounter() {
+    
+      let cards = document.getElementById('conta').style.display="inline";
+      counterElement.textContent = counter;
+      timeoutId = setTimeout(function() {
+        counter--;
+        counterElement.textContent = counter;
+        if (counter > 0) {
+          startCounter();
+        }else
+        {
+            counterElement.textContent = 'finish'
+            let cards = document.getElementById('conta').style.display="none";
+            counter = 5
+        }
+      }, 1000);
+
+      if(counter == 5)
+      {
+        changeIcon()
+      }
+      
+      
+    }
+
+    function cheakValue()
+    {
+      var el = document.querySelector('input[name="group1"]:checked').value
+      
+      if(el == randomIcon)
+      {
+        score++
+        let elScore = document.getElementById('score').innerHTML = score
+        
+        alert('is true')
+        
+        radios = document.querySelectorAll('#radio')
+        for (const radio of radios) {
+          radio.checked = false
+        }
+      }else
+      {
+        score--
+        let elScore = document.getElementById('score').innerHTML = score
+        alert('is false')
+        // un Checked
+        
+        radios = document.querySelectorAll('#radio')
+        for (const radio of radios) {
+          radio.checked = false
+        }
+      }
+    }
+
+    function stopCounter() {
+      clearTimeout(timeoutId);
+    }
